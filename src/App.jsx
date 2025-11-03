4503edfe28a17433bb5c7de12de92e018471126a
LegendaryAman <161002501+LegendaryAman@users.noreply.github.com>
2025-11-03 12:01:57 +0530

    implement body of page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-
+import backgroundImage from './assets/images/fooddeliverydriverimg.png'
 function App() {
   return (
     <>
@@ -40,12 +40,16 @@ function App() {
         <div className="hero-text">
           <h1>Deliver <span className="content_span">Your Food</span></h1>
           <h1>Easy and Fast</h1>
-          <p className="content_alice">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, amet.</p>
-        </div>
-        <form>
+          <p className="content_alice">Fresh, hot, and at your door in 30 minutes — because hunger shouldn’t wait</p>
+          <form>
         <input type="text" placeholder='Enter your delivery' />
         <button type='submit' className='btn_sec'>Search</button>
       </form>
+        </div>
+       
+      <div className="hero_image">
+        <img src={backgroundImage} alt="image not found" className="image" />
+      </div>
       </div>
     
     </div>
